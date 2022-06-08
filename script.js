b8cca89e5a200ff6fff0e9fb4d1424b40a897c71
eugenelye <eugenelye91@gmail.com>
2022-06-08 14:45:04 +0800

    day3

--- script.js
+++ script.js
@@ -4,23 +4,11 @@ const ctx = canvas.getContext("2d");
 window.addEventListener("keydown", keyDown);
 
 //creating the start button
-const startButton = document.createElement('button');
-startButton.className = 'start';
-startButton.innerText = 'Start Game!';
-document.querySelector('body').append(startButton);
-startButton.addEventListener("click", startGame);
-
-//onclick function to start the game
-function startGame () {
-   setInterval(fallingRect, 30);
-};
-
-//this defines the button width size, which is 1/3 width of the canvas
-const buttonWidth = [canvas.width / 3, canvas.width / 3, canvas.width / 3];
-const buttonHeight = [canvas.width / 16, canvas.width / 16, canvas.width / 16];
+document.querySelector('.start').addEventListener("click", startGame);
+document.querySelector('.restart').addEventListener("click", restartGame);
 
 //this defines the button colors
-const colors = ["red", "blue", "green"];
+const colors = ["red", "blue", "green","yellow", "cyan"];
 
 //this push the buttons into an array
 const rectangle = [];
@@ -28,7 +16,40 @@ const rectangle = [];
 //inital conditions of the game
 let score = 0;
 let speed = 3;
-let time = 5;
+let time = 10;
+
+//onclick function to start the game
+function restartGame () {
+  
+  document.getElementById("canvas").style.display = "inital";
+  setInterval(fallingRect,30);
+  
+
+}
+
+function startGame () {
+
+  setInterval(fallingRect,30);
+
+
+  }
+
+let countDown = setInterval(function(){
+  time--;
+  if (time === 0) {
+    clearInterval(countDown);
+    alert(`Time is up! Your score is ${score}!`);
+    document.getElementById("canvas").style.display = "none";
+
+  }
+
+
+}, 1000);
+
+//this defines the button width size, which is 1/3 width of the canvas
+const buttonWidth = [canvas.width / 5, canvas.width / 5, canvas.width / 5, canvas.width / 5, canvas.width / 5];
+const buttonHeight = [canvas.width / 16, canvas.width / 16, canvas.width / 16, canvas.width / 16, canvas.width / 16];
+
 
 //this function sets the text properties
 function text(message, x, y) {
@@ -38,17 +59,19 @@ function text(message, x, y) {
 
 //this function draw the buttons at the bottom.
 function drawButtons() {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 5; i++) {
     ctx.beginPath();
     //this function color the rectangle and set the rectangle properties at the bottom.
     ctx.fillStyle = colors[i];
-    ctx.fillRect((i * canvas.width) / 3, canvas.height-buttonHeight[i], buttonWidth[i], buttonHeight[i]);
+    ctx.fillRect((i * canvas.width) / 5, canvas.height-buttonHeight[i], buttonWidth[i], buttonHeight[i]);
   }
   //this function draw the button text at the bottom.
   ctx.font = "50px Arial";
-  text("z", 0 + 100, canvas.height);
-  text("x", (1 * canvas.width / 3) + 100, canvas.height);
-  text("c", (2 * canvas.width / 3) + 100, canvas.height);
+  text("z", 0 + (canvas.width/10), canvas.height);
+  text("x", (1 * canvas.width / 5) + (canvas.width/10), canvas.height);
+  text("c", (2 * canvas.width / 5) + (canvas.width/10), canvas.height);
+  text("v", (3 * canvas.width / 5) + (canvas.width/10), canvas.height);
+  text("b", (4 * canvas.width / 5) + (canvas.width/10), canvas.height);
 }
 
 function drawRect() {
@@ -57,9 +80,9 @@ function drawRect() {
     ctx.beginPath();
     ctx.fillStyle = colors[rectangle[i][0]];
     ctx.fillRect(
-      (rectangle[i][0] * canvas.width) / 3,
+      (rectangle[i][0] * canvas.width) / 5,
       rectangle[i][1],
-      canvas.width / 3,
+      canvas.width / 5,
       50
     );
   }
@@ -69,27 +92,32 @@ function makeRect() {
   // This function will random set a number to the blocks[] (0 = red, 1 = blue, 2 = green). 
   // if red is randomised, it will be placed at the left. position = 0 will be assigned to the rectangle y position every time. 
   // - Note: This will be call every 3000/speed milliseconds
-  rectangle.push([Math.round(Math.random() * 3), 0]);
+  rectangle.push([Math.floor(Math.random() * 5), 0]);
   setTimeout(makeRect, 3000 / speed);
 }
 
+
+// This function will animate the falling rectangle block on the screen
+// - Note: This will be call every 30 milliseconds -> setInterval of fallingRect at 30 ms. 
 function fallingRect() {
 
   //these 2 below shows the speed and add the score to be shown to the user.
-  document.getElementById("speed").innerHTML = "Speed: " + speed;
+  document.getElementById("speed").innerHTML = "Speed: " + speed ;
   document.getElementById("score").innerHTML = "Score: " + score;
   document.getElementById("time").innerHTML = "Time: " + time;
 
-  // This function will animate the falling rectangle block on the screen
-  // - Note: This will be call every 30 milliseconds -> setInterval of fallingRect at 30 ms. 
-
+  
   for (let i = 0; i < rectangle.length; i++) {
+
   
   //this makes the rectangle fall at the assigned speed. the y position of the rectangle changes by 2 px at every 30ms. 
     rectangle[i][1] += speed;
   
+
+    
+  
   // when the rectangle made goes beyond half of the button height, it is deleted. 
-    if (rectangle[i][1] > 750) {
+    if (rectangle[i][1] > (canvas.height - buttonHeight[i])) {
       rectangle.splice(i, 1);
     }
     // This function will clear the rectangle on the previous pixel, making the moving effect
@@ -140,4 +168,25 @@ function keyDown() {
       }
     }
   }
+
+  if (key == "v") {
+    if (rectangle[0][0] == 3) {
+      if (rectangle[0][1] >= 700) {
+        score += 1;
+        rectangle.splice(0, 1);
+      }
+    }
+  }
+
+  if (key == "b") {
+    if (rectangle[0][0] == 4) {
+      if (rectangle[0][1] >= 700) {
+        score += 1;
+        rectangle.splice(0, 1);
+      }
+    }
+  }
+
+
 }
+
